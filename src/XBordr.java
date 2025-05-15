c3b728022030a4c54e91dee64cbc61ddede1fd18
Praveen <praveen.sinu1995@gmail.com>
2025-05-15 17:31:58 +0530

    first commit

---
+++ src/XBordr.java
@@ -0,0 +1,24 @@
+public class XBordr {
+    public static void main(String[] args) {
+        int n=15;
+        for (int i = 0; i <n/2 ; i++) {
+            for (int j = 0; j <n; j++) {
+                if(i>=j||i+j>=n-1)
+                {
+                    System.out.print('*');
+                }else System.out.print(' ');
+
+            }
+            System.out.println();
+        }
+        for (int i = n/2-1; i >=0 ; i--) {
+
+            for (int j = 0; j <n ; j++) {
+                if (i>=j||i+j>=n-1){
+                    System.out.print('*');
+                }else System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+}
